5bc0f0904ea8806636fbade744121c351f6d1b3c
Harshita <harshitabhadimane02051998@gmail.com>
2024-09-29 15:57:56 +0530

    Functions.

--- 04.function.sh
+++ 04.function.sh
@@ -3,5 +3,14 @@ example() {
 }
 
 
-#call function
+# call function
 example
+
+# Functions have its own variables
+
+example1() {
+  echo value of 1 - $1
+  echo "value of * - $*"
+  echo "value of # - $#"
+}
+ example1 10 20 30
