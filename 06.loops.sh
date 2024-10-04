26999983148d353d4cfb90b3326c9788af22c12f
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-04 13:02:55 +0530

    While loop. For loop.

--- 06.loops.sh
+++ 06.loops.sh
@@ -1,7 +1,11 @@
-# While loop
+# While loop workd on expression
 i=0
 while [ $i -lt 10 ]; do
   echo i - $i
   i=$(($i+1))
 done
 
+# For loop
+for fruit in apple banana orange ; do
+  echo fruit - $fruit
+done
