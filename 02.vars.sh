fcee426dd73da14c5197c91f7962a720e38b74be
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-04 13:16:01 +0530

    While loop. For loop. Command Sub. Arithmetic Sub.

--- 02.vars.sh
+++ 02.vars.sh
@@ -6,3 +6,14 @@ b="Hello World"
 
 echo $a
 echo $b
+
+# Substitution
+Command Sub
+# A command output if we want in a variable the command sub
+# syntax : var=$(command)
+x=$(uptime)
+
+Arithmetic Sub
+# Arithmetic expression solved and output to a variable then command sub
+y=$((2+3))
+z=$((2+3-5/2*10))
