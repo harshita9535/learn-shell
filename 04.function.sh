9743d7ac1336b93a413282c4045d10e0c2af5beb
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-04 13:27:24 +0530

    example3

--- 04.function.sh
+++ 04.function.sh
@@ -13,5 +13,14 @@ example1() {
   echo "value of * - $*"
   echo "value of # - $#"
 }
- example1 10 20 30
- example1 "$*"
+example1 10 20 30
+example1 "$*"
+
+example3() {
+  echo Hello
+  return 10
+  echo Bye
+}
+
+example3
+echo Return status - $?
